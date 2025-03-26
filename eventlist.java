d9cbf6e0da92eda4f9efbaf54f8b2260341152a6
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-03-26 18:03:02 +0800

    started

---
+++ eventlist.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class eventlist{
+    public static void main(String[] args) {
+        JFrame wholelist = new JFrame();
+        wholelist.setVisible(true);
+        wholelist.setSize(440,540);
+        wholelist.setTitle("Events List");
+        wholelist.setAlwaysOnTop(true);
+        wholelist.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon progIcon = new ImageIcon("pictures/program_icon.png");  
+        wholelist.setIconImage(progIcon.getImage());  
+        wholelist.getContentPane().setBackground(new Color(0,1,1));   
+    }
+}
