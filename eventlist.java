dd2a648cffa2df7b180edda61d3e3b715404d4af
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-03-26 19:06:16 +0800

    bro wtf have i built

--- eventlist.java
+++ eventlist.java
@@ -1,19 +1,29 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.HeadlessException;
 import java.awt.Image;
+import java.awt.Label;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
 
-public class eventlist{
-    public static void main(String[] args) {
-        JFrame wholelist = new JFrame();
-        wholelist.setVisible(true);
-        wholelist.setSize(750,540);
-        wholelist.setTitle("Events List");
-        wholelist.setAlwaysOnTop(true);
-        wholelist.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon progIcon = new ImageIcon("pictures/program_icon.png");  
-        wholelist.setIconImage(progIcon.getImage());  
-        wholelist.getContentPane().setBackground(new Color(128, 0, 128));   
-    }
+public class Eventlist extends JFrame {
+
+  Eventlist() {
+    JLabel txt = new JLabel("Damn im a txt string");
+    txt.setLayout(new BorderLayout());
+    this.add(txt, BorderLayout.CENTER);
+    this.setVisible(true);
+    this.setSize(750, 540);
+    this.setTitle("Events List");
+
+    this.setAlwaysOnTop(true);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ImageIcon progIcon = new ImageIcon("pictures/program_icon.png");
+    this.setIconImage(progIcon.getImage());
+    this.getContentPane().setBackground(new Color(123, 50, 250));
+    this.add(txt);
+  }
 }
---
+++ main.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class main{
+    public static void main(String[] args) {
+         new Eventlist();// launch eventlist
+    }
+} 
